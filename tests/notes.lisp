c4e9596443eeb6213eea775cd7f91599934ba801
Gary King <gwking@metabang.com>
2006-09-12 12:06:52 -0400

    Added more tests
    darcs-hash:20060912160652-3cc5d-620e9737823771f15fdc7170e8d7cb5dbdd4af93.gz

---
+++ tests/notes.lisp
@@ -0,0 +1,40 @@
+test that temporary files are cleaned up
+
+test with no gpg
+
+test for existence of correct restarts
+
+test for different locations
+
+ok - install non-existent package
+
+install package with no dependencies
+
+install package with several dependencies
+
+install two packages (or more) at once
+
+test propagation
+
+make sure only the requested packages are installed
+
+test trusted uids
+
+test proxy
+
+test preferences
+
+meta: globals, main functions, etc.
+  *proxy*, *proxy-user*, *proxy-passwd*
+  *trusted-ids*
+  *verify-gpg-signatures*
+  *safe-url-prefixes*
+  *preferred-location*
+  *cclan-mirror*
+  *cygwin-bash-program*, *gnu-tar-program*
+  *asdf-install-dirs*
+  *private-asdf-install-dirs*
+  *locations*
+  *propagate-installation*
+  *tar-extractors*
+
---
+++ tests/unit-tests.lisp
@@ -0,0 +1,43 @@
+(in-package test-asdf-install)
+
+(deftestsuite test-install-location (test-asdf-install) ())
+
+(addtest (test-install-location)
+  (let ((*preferred-location* -34))
+    (ensure-error (asdf-install::install-location))))
+
+(addtest (test-install-location)
+  (let ((*preferred-location* 'hello))
+    (ensure-error (asdf-install::install-location))))
+
+(addtest (test-install-location)
+  (let ((*preferred-location* 'home)
+	(*locations* 
+	 `((a a home)
+	   (b b away)
+	   (c c far))))
+    (ensure-same (asdf-install::install-location) '(a a home) :test #'equal)))
+
+(addtest (test-install-location)
+  (let ((*preferred-location* 'away)
+	(*locations* 
+	 `((a a home)
+	   (b b away)
+	   (c c far))))
+    (ensure-same (asdf-install::install-location) '(b b away) :test #'equal)))
+
+(addtest (test-install-location)
+  (let ((*preferred-location* 3)
+	(*locations* 
+	 `((a a home)
+	   (b b away)
+	   (c c far))))
+    (ensure-same (asdf-install::install-location) '(c c far) :test #'equal)))
+
+(addtest (test-install-location)
+  (let ((*preferred-location* 'far)
+	(*locations* 
+	 `((a a home)
+	   (b b away)
+	   (c c far))))
+    (ensure-same (asdf-install::install-location) '(c c far) :test #'equal)))
