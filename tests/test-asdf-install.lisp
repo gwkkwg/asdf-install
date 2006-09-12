e14f8a14a25c08c091ef29c1c19a8a14b019e186
Gary King <gwking@metabang.com>
2006-09-12 12:07:55 -0400

    More tests
    darcs-hash:20060912160755-3cc5d-4e9a3f2c1b5fc1dec2bf8babf1d9dbadb9d8ca87.gz

--- tests/test-asdf-install.lisp
+++ tests/test-asdf-install.lisp
@@ -1,62 +1,75 @@
-#|
-install non-existence package
-
-install package with no dependencies
-
-install package with several dependencies
-
-make sure only the requested packages are installed
-
-test with / without verification
-
-test trusted uids
-
-test proxy
-
-|#
-
-(in-package #:common-lisp-user)
-(defpackage #:test-asdf-install
-  (:use #:common-lisp #:asdf-install #:lift #:cl-fad))
 (in-package #:test-asdf-install)
 
 (defvar *working-directory*
   (translate-logical-pathname "user-home:temporary;test-asdf-install;"))
 
-(deftestsuite test-asdf-install () 
-  ((old-locations *locations*)
-   (old-prefered *preferred-location* )
-   (old-registry asdf:*central-registry*))
-  (:setup (setf *locations*
-                `((,(merge-pathnames (make-pathname :directory '(:relative "site"))
-                                     *working-directory*)
-                   ,(merge-pathnames (make-pathname :directory '(:relative "site-systems"))
-                                     *working-directory*)
-                   "temporary install"))
-                *preferred-location* 0
-                asdf:*central-registry* 
-                (list 
-                 (merge-pathnames (make-pathname :directory '(:relative "site-systems"))
-                                  *working-directory*))))
-  (:teardown (setf *locations* old-locations
-                   *preferred-location* old-prefered
-                   asdf:*central-registry* old-registry)))
-
-(deftestsuite test-asdf-install-no-verify (test-asdf-install) 
-  ((old-verify *verify-gpg-signatures*))
-  (:setup (setf *verify-gpg-signatures* nil)
-          (delete-directory-and-files *working-directory* :if-does-not-exist :ignore))
-  (:teardown (setf *verify-gpg-signatures* old-verify)))
+(deftestsuite test-asdf-install ()
+  ()
+  (:dynamic-variables 
+   (*locations* 
+   `((,(merge-pathnames (make-pathname :directory '(:relative "site"))
+			*working-directory*)
+       ,(merge-pathnames (make-pathname :directory '(:relative "site-systems"))
+			 *working-directory*)
+       "temporary install")))
+   (*preferred-location* "temporary install")
+   (asdf:*central-registry* 
+   (list 
+    (merge-pathnames (make-pathname :directory '(:relative "site-systems"))
+		     *working-directory*)))))
 
 (addtest (test-asdf-install-no-verify)
   test-non-existent-package
   (ensure-condition 'download-error
     (install 'i-hope-this-package-does-not-exist)))
 
-(addtest (test-asdf-install-no-verify)
+(deftestsuite test-asdf-install-basic-installation (test-asdf-install) 
+  ()
+  (:dynamic-variables 
+   (*verify-gpg-signatures* t))
+  (:setup 
+   (delete-directory-and-files *working-directory* :if-does-not-exist :ignore)))
+
+(addtest (test-asdf-install-basic-installation)
+  test-non-existent-package
+  (ensure-condition 'download-error
+    (install 'i-hope-this-package-does-not-exist)))
+
+(addtest (test-asdf-install-basic-installation)
+  test-download-two-at-once
+  (let ((handled-error-count 0))
+    (handler-bind 
+	((gpg-error (lambda (c) 
+		      (incf handled-error-count)
+		      (let ((r (find-restart 'install-anyways)))
+			(when r (invoke-restart r)))
+		      (error c))))
+      (ensure-same (install '(lw-compat asdf-binary-locations))
+		   '(lw-compat asdf-binary-locations))
+      (ensure-same handled-error-count 2))))
+
+(deftestsuite test-asdf-install-no-gpg-verification (test-asdf-install) 
+  ()
+  (:dynamic-variables 
+   (*verify-gpg-signatures* nil))
+  (:setup 
+   (delete-directory-and-files *working-directory* :if-does-not-exist :ignore)))
+
+(addtest (test-asdf-install-no-gpg-verification)
+  test-non-existent-package
+  (ensure-condition 'download-error
+    (install 'i-hope-this-package-does-not-exist)))
+
+(addtest (test-asdf-install-no-gpg-verification)
   test-simple-download
   (install 'lw-compat))
 
-(addtest (test-asdf-install-no-verify)
+(addtest (test-asdf-install-no-gpg-verification)
   test-download-with-dependencies
   (install 'moptilities))
+
+(addtest (test-asdf-install-no-gpg-verification)
+  test-download-two-at-once
+  (ensure-same (install '(lw-compat asdf-binary-locations))
+	       '(lw-compat asdf-binary-locations)))
+
