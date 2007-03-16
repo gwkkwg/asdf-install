187f371132beaada8cefba025cdd2c1a904b47cb
Gary King <gwking@metabang.com>
2007-03-16 12:18:19 -0400

    Added test-with-tar-file
    darcs-hash:20070316161819-3cc5d-47fe3e6b6d90ff053e709a00401c93f4562a95b4.gz

--- tests/test-asdf-install.lisp
+++ tests/test-asdf-install.lisp
@@ -68,3 +68,39 @@
   (ensure-same (install '(lw-compat asdf-binary-locations))
 	       '(lw-compat asdf-binary-locations)))
 
+;;;;;
+
+(deftestsuite test-with-tar-file (test-asdf-install)
+  ())
+
+(addtest (test-with-tar-file)
+  (let ((result 
+	 (install
+	  (namestring (asdf:system-relative-pathname 
+		       'test-asdf-install 
+		       "tests/data/log5_latest.tar.gz")))))
+    (ensure-same (length result) 2)
+    (ensure (member 'log5 result :test 'string-equal))
+    (ensure (member 'log5-test result :test 'string-equal))))
+
+#|
+(asdf-install::local-archive-p 
+ (namestring (asdf:system-relative-pathname 
+	      'test-asdf-install 
+	      "tests/data/log5_latest.tar.gz")))
+
+(install
+ (namestring (asdf:system-relative-pathname 
+	      'test-asdf-install 
+	      "tests/data/log5_latest.tar.gz"))
+ :where 2)
+
+
+(install
+ 'log5
+ :where 2)
+
+(asdf-install::local-archive-p (string 'log5))
+
+(trace asdf-install::install-package)
+|#
