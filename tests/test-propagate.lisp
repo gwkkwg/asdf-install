18d8ef701c68fcd62d8db15ad5e30c5b66f5fa7d
Gary King <gwking@metabang.com>
2007-12-31 19:12:54 -0500

    Added new test file
    darcs-hash:20080101001254-3cc5d-5358f97dcc445476e7f518108d35ed372dfd8c40.gz

---
+++ tests/test-propagate.lisp
@@ -0,0 +1,23 @@
+(in-package #:test-asdf-install)
+
+(deftestsuite test-propagate (test-asdf-install)
+  ()
+  (:dynamic-variables 
+   (*verify-gpg-signatures* nil))
+  (:run-setup :once-per-suite)
+  (:setup
+   (install 'moptilities)))
+
+#+(or)
+;;?? this might make sense if/when we do some sort of version checking / upgrade
+(addtest (test-propagate)
+  donot-download-unless-required
+  (ensure-install-results-same
+   '()
+   (install 'moptilities)))
+
+(addtest (test-propagate)
+  propagate-gets-everything
+  (ensure-install-results-same
+   '(moptilities closer-mop lw-compat)
+   (install 'moptilities :propagate t)))
