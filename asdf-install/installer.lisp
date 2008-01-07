7faeb69493c450ec8c630300c31765572ec47cf7
Gary King <gwking@metabang.com>
2008-01-07 09:13:19 -0500

    Moved a few platform specific forms (e.g., tar-argument) from installer to port
    darcs-hash:20080107141319-3cc5d-2205228ddee45a9a86a80fd6cdefa7cba39b0b5f.gz

--- asdf-install/installer.lisp
+++ asdf-install/installer.lisp
@@ -313,25 +313,6 @@ include the filename and extension, if applicable."
 (defun tar-command ()
   (find-program *gnu-tar-program*))
 
-(defun tar-argument (arg)
-  "Given a filename argument for tar, massage it into our guess of the
- correct form based on the feature list."
-  #-(or :win32 :mswindows :scl)
-  (namestring (truename arg))
-  #+scl
-  (ext:unix-namestring (truename arg))
-  
-  ;; Here we assume that if we're in Windows, we're running Cygwin,
-  ;; and cygpath is available. We call out to cygpath here rather than
-  ;; using shell backquoting. Relying on the shell can cause a host of
-  ;; problems with argument quoting, so we won't assume that
-  ;; RETURN-OUTPUT-FROM-PROGRAM will use a shell. [dwm]
-  #+(or :win32 :mswindows)
-  (with-input-from-string (s (return-output-from-program
-                              (find-program "cygpath.exe")
-                              (list (namestring (truename arg)))))
-    (values (read-line s))))
-
 (defun extract-using-tar (to-dir tarball)
   (let ((tar-command (tar-command)))
     (if tar-command
@@ -530,11 +511,6 @@ the package."
       (dolist (l *temporary-files* t)
         (when (probe-file l) (delete-file l))))
     (nreverse *systems-installed-this-time*)))
-
-(defun local-archive-p (package)
-  #+(or :sbcl :allegro) (probe-file package)
-  #-(or :sbcl :allegro) (and (not (looks-like-url-p package))
-                             (probe-file package)))
 
 (defun looks-like-url-p (string)
   (= (mismatch string "http://") 7))
--- asdf-install/port.lisp
+++ asdf-install/port.lisp
@@ -476,6 +476,31 @@
 and NIL NAME and TYPE components"
   (make-pathname :name nil :type nil :defaults pathname))
 
+(defun tar-argument (arg)
+  "Given a filename argument for tar, massage it into our guess of the
+ correct form based on the feature list."
+  #-(or :win32 :mswindows :scl)
+  (namestring (truename arg))
+  #+scl
+  (ext:unix-namestring (truename arg))
+  
+  ;; Here we assume that if we're in Windows, we're running Cygwin,
+  ;; and cygpath is available. We call out to cygpath here rather than
+  ;; using shell backquoting. Relying on the shell can cause a host of
+  ;; problems with argument quoting, so we won't assume that
+  ;; RETURN-OUTPUT-FROM-PROGRAM will use a shell. [dwm]
+  #+(or :win32 :mswindows)
+  (with-input-from-string (s (return-output-from-program
+                              (find-program "cygpath.exe")
+                              (list (namestring (truename arg)))))
+    (values (read-line s))))
+
+(defun local-archive-p (package)
+  #+(or :sbcl :allegro) (probe-file package)
+  #-(or :sbcl :allegro) (and (not (looks-like-url-p package))
+                             (probe-file package)))
+
+
 ;;; Local variables:
 ;;; indent-tabs-mode:nil
 ;;; End:
